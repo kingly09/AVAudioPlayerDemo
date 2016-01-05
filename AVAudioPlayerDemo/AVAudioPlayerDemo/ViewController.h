0b126e531f5bbcec6d22a73b7b0e86eff7e7e160
kingly <kingly09@gmail.com>
2016-01-05 17:39:04 +0800

    使用AVAudioPlayer播放音乐，并且 可控制音量，控制速率，立体声，可以暂停，可以重置设置

---
+++ AVAudioPlayerDemo/AVAudioPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AVAudioPlayerDemo
+//
+//  Created by kingly on 16/1/5.
+//  Copyright © 2016年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AVAudioPlayerDemo/AVAudioPlayerDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AVAudioPlayerDemo
+//
+//  Created by kingly on 16/1/5.
+//  Copyright © 2016年 kingly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
